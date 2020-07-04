20caa3a45329acf1b005ceaccee9de8efc9ff12d
Ohjun Kwon <dev@kojandy.com>
2020-07-04 13:09:20 +0900

    Update polybar

--- config/polybar/playerctl.sh
+++ config/polybar/playerctl.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-if [ "$(playerctl status 2> /dev/null)" = "Playing" ]; then
-    echo "▶️ "$(playerctl metadata title 2> /dev/null)
+player_status=$(playerctl status 2> /dev/null)
+title=$(playerctl metadata title 2> /dev/null)
+artist=$(playerctl metadata artist 2> /dev/null)
+
+if [ ! "$artist" = "" ]; then
+    title=$artist' - '$title
+fi
+
+if [ "$player_status" = "Playing" ]; then
+    echo " ""$title"
+elif [ "$player_status" = "Paused" ]; then
+    echo " ""$title"
 else
     echo ""
 fi
